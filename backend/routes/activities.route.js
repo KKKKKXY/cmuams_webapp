f254199be6cfbb2b25a3f630bbb0cb5ace38544c
fat_cola <kehanzhou97@gmail.com>
2020-07-18 16:33:39 +0700

    add a button for delete activity

--- backend/routes/activities.route.js
+++ backend/routes/activities.route.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 // import controller
 const { requireSignin } = require('../controllers/auth.controller');
-const { ListController,readController,AddController } = require('../controllers/activity.controller');
+const { ListController,readController,AddController,deleteController} = require('../controllers/activity.controller');
 
 router.get('/activities', ListController);
 router.get('/activity/:id', requireSignin, readController);
 router.post('/activity/add', AddController);
+router.delete('/activity/:id', deleteController);
 
 
 module.exports = router;
