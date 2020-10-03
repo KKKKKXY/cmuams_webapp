b7f7e95e7fd06645e77ce083d598c2e6ba516e9b
Mya <mya@192.168.0.16>
2020-10-03 14:13:05 +0700

    add/bidActivityRound1Popup

--- src/screens/StuViewActivity.jsx
+++ src/screens/StuViewActivity.jsx
@@ -1,46 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import PrivateNavbar from './PrivateNavbar';
 import Tooltip from '@material-ui/core/Tooltip';
 import { isAuth, setActivityLocalStorage } from '../helpers/auth';
 import { ToastContainer, toast } from 'react-toastify';
+import Popup from "../helpers/Popup";
+import BidForm from "./BidForm"
+import * as bidActivityService from "../services/BidActivityService";
 
-const Activities = props => (
-    <tr>
-        <td>{props.activity.activityName}</td>
-        <td>{props.activity.description}</td>
-        <td>{props.activity.startDate}</td>
-        <td>{props.activity.bidEndDate}</td>
-        <td>{props.activity.location}</td>
-        <td>{props.activity.responsiblePerson}</td>
-        <td>{props.activity.phoneNo}</td>
-        <td>{props.activity.limitParticipant}</td>
-        <td>
-            <Tooltip title="Enroll" placement="top">
-                <a href="#" onClick={() => { props.enrollActivity(props.activity._id) }}><i className="fas fa-plus-square" aria-hidden="true"></i></a>
-            </Tooltip>
-        </td>
-    </tr>
-)
+const Activities = props => {
+    const [openPopup, setOpenPopup] = useState(false)
+    const [records, setRecords] = useState(bidActivityService.getAllTransfers())
+    const bid = (transter, resetForm) => {
+        if (transter.id == 0)
+            bidActivityService.insertTransfer(transter)
+        resetForm()
+        setOpenPopup(false)
+        setRecords(bidActivityService.getAllTransfers())
+    }
+
+    return (
+        <>
+            < tr >
+                <td>{props.activity.activityName}</td>
+                <td>{props.activity.description}</td>
+                <td>{props.activity.startDate}</td>
+                <td>{props.activity.bidEndDate}</td>
+                <td>{props.activity.location}</td>
+                <td>{props.activity.responsiblePerson}</td>
+                <td>{props.activity.phoneNo}</td>
+                <td>{props.activity.limitParticipant}</td>
+                <td>
+                    <Tooltip title="Enroll" placement="top">
+                        <a href="#" onClick={() => { setOpenPopup(true); }}><i className="fas fa-plus-square" aria-hidden="true"></i></a>
+                        {/* <a href="#" onClick={() => { props.enrollActivity(props.activity._id) }}><i className="fas fa-plus-square" aria-hidden="true"></i></a> */}
+                    </Tooltip>
+                </td>
+            </tr >
+
+            <Popup
+                title="Bidding Activity Transfer"
+                openPopup={openPopup}
+                setOpenPopup={setOpenPopup}
+            >
+                <BidForm bid={bid}  options={props.catchActivity()}/>
+            </Popup>
+        </>
+    )
+}
+
+function Receiver(id, name) {
+    this.id = id;
+    this.name = name;
+}
 
 export default class StuViewActivity extends Component {
+
     constructor(props) {
         super(props);
-        this.editActivity = this.enrollActivity.bind(this);
+        this.enrollActivity = this.enrollActivity.bind(this);
+        this.catchActivity = this.catchActivity.bind(this);
         this.state = {
-            activities: []
+            activities: [],
+            options: []
         };
     }
 
     componentDidMount() {
         axios.get(`${process.env.REACT_APP_API_URL}/activities`)
             .then(response => {
-                this.setState({ activities: response.data })
-                console.log(response.data);
+                let tmpArray = []
+                for (var i = 0; i < response.data.length; i++) {
+                    tmpArray.push(new Receiver((i + 1).toString(), response.data[i].activityName))
+                }
+                this.setState({
+                    activities: response.data,
+                    options: tmpArray
+                })
+                console.log('componentDidMount()');
+                console.log(this.state.options);
+
             })
             .catch((error) => {
                 console.log(error);
             })
+    }
+
+    catchActivity() {
+        return this.state.options;
     }
 
     enrollActivity(activityId) {
@@ -58,7 +105,7 @@ export default class StuViewActivity extends Component {
 
     activityList() {
         return this.state.activities.map(currentactivity => {
-            return <Activities activity={currentactivity} key={currentactivity._id} enrollActivity={this.enrollActivity} />;
+            return <Activities activity={currentactivity} key={currentactivity._id} catchActivity={this.catchActivity} enrollActivity={this.enrollActivity} />;
         })
     }
 
