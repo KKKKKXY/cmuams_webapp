e92f0589c9ca96af64f839c3e7ce97e6d74e4bd5
fat_cola <kehanzhou97@gmail.com>
2020-10-07 19:00:11 +0700

    fix the project files

--- src/screens/Admin Screens/Admin.jsx
+++ src/screens/Admin Screens/Admin.jsx
@@ -133,20 +133,6 @@ const Admin = ({ history }) => {
                   </button>
                 </div>
                 <div className='my-12 border-b text-center'>
-                  <div className='leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
-                    Go To Home
-                </div>
-                </div>
-                <div className='flex flex-col items-center'>
-                  <a
-                    className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
-           bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
-                    href='/'
-                    target='_self'
-                  >
-                    <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-indigo-500' />
-                    <span className='ml-4'>Home</span>
-                  </a>
                 </div>
               </form>
             </div>
